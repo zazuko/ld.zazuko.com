294cdf17713e19a54936b745dda4c18bdc9778ae
Ludovic Muller <ludovicmuller1@gmail.com>
2022-12-06 16:22:01 +0100

    feat: enable rewriting + improve local test environment

--- start-dev.sh
+++ start-dev.sh
@@ -1,4 +1,4 @@
-export SPARQL_ENDPOINT_URL=http://localhost:3030/test-data/query
-export SPARQL_ENDPOINT_USER=public
-export SPARQL_ENDPOINT_PASSWORD=public
+export SPARQL_ENDPOINT_URL=https://ld.zazuko.com/query
+export SPARQL_ENDPOINT_USER
+export SPARQL_ENDPOINT_PASSWORD
 DEBUG=http,trifid:* npm start
